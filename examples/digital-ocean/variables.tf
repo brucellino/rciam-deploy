e2dfae9a53479fe18d8893592801cbf221f7a093
Bruce Becker <brucellino@protonmail.com>
2024-12-09 11:17:22 +0100

    chore(do): initialise terraform for example (#5)
    * chore(do): initialise terraform for example
    
    Signed-off-by: Bruce Becker <bruce.becker@egi.eu>
    
    * ci: run CI on PRs to devel and master
    
    Signed-off-by: Bruce Becker <bruce.becker@egi.eu>
    
    * ci: run lint with backwards compat as well as latest
    
    Signed-off-by: Bruce Becker <bruce.becker@egi.eu>
    
    * ci: use matrix version in CI
    
    Signed-off-by: Bruce Becker <bruce.becker@egi.eu>
    
    * ci: so much for github workflow validation
    
    Signed-off-by: Bruce Becker <bruce.becker@egi.eu>
    
    * ci: what's up with matrix
    
    Signed-off-by: Bruce Becker <bruce.becker@egi.eu>
    
    * chore: check if it's the @$ that's breaking
    
    Signed-off-by: Bruce Becker <bruce.becker@egi.eu>
    
    * ci: bump version to 24.2.1
    
    Signed-off-by: Bruce Becker <bruce.becker@egi.eu>
    
    * ci: fix task name
    
    Signed-off-by: Bruce Becker <bruce.becker@egi.eu>
    
    * build(do): add database and user
    
    Signed-off-by: Bruce Becker <bruce.becker@egi.eu>
    
    * build(do): add droplet and db firewall
    
    Signed-off-by: Bruce Becker <bruce.becker@egi.eu>
    
    ---------
    
    Signed-off-by: Bruce Becker <bruce.becker@egi.eu>

---
+++ examples/digital-ocean/variables.tf
@@ -0,0 +1,35 @@
+variable "deployment_name" {
+  type        = string
+  description = "Name of the deployment of this instance"
+  default     = "default"
+}
+
+variable "region" {
+  type        = string
+  description = "Name of the DigitalOcean region we are creating the VPC in"
+  default     = "ams3"
+}
+
+variable "pg_version" {
+  type        = string
+  description = "Postgres version of managed DB"
+  default     = "11"
+}
+
+variable "db_size" {
+  type        = string
+  description = "Instance size for PostgreSQL db."
+  default     = "db-s-1-vcpu-1gb"
+}
+
+variable "db_user" {
+  type        = string
+  description = "Username to connect to DB"
+  default     = "postgres"
+}
+
+variable "db_name" {
+  type        = string
+  description = "Name fo the PostGres database for RCIAM"
+  default     = "rciam"
+}
